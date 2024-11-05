14c2dc77fa146bf8a9a59c0cbbec660b88b6fb31
lucas-s-p <lucas.pereira@ccc.ufcg.edu.br>
2024-11-05 13:32:55 -0300

    [Refactor] Modify granite model file settings

--- opencompass/configs/models/hf_granite/hf_granite3_8b_chat.py
+++ opencompass/configs/models/hf_granite/hf_granite3_8b_chat.py
@@ -5,8 +5,9 @@ models = [
         type=HuggingFacewithChatTemplate,
         abbr='granite-3.0-8b-instruct',
         path='ibm-granite/granite-3.0-8b-instruct',
-        max_out_len=2048,
+        max_out_len=1024,
         batch_size=8,
         run_cfg=dict(num_gpus=1),
+        stop_words=['<|end_of_text|>', '<|eot_id|>'],
     )
 ]
